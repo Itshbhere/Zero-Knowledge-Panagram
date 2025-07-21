fbd97332bd026de1d503a65fd026eb8cd52ae118
Hafiz Burhan <larsalaxsanderson@gmail.com>
2025-07-21 13:29:31 +0500

    Panagramed

--- contract/js-scripts/generateProof.ts
+++ contract/js-scripts/generateProof.ts
@@ -1,12 +1,57 @@
 import { Noir } from "@noir-lang/noir_js";
-import {ethers} from "ethers";
-import {UltraHonkBackend} from "@aztec/bb.js";
+import { ethers } from "ethers";
+import { UltraHonkBackend } from "@aztec/bb.js";
+import { fileURLToPath } from "url";
+import path from "path";
+import fs from "fs";
 
 //////--------------Steps to generate a proof------------------//////
 // 1. Get The Circuit File
-// 2. Initialize Noir with the Circuit File
-// 3. Initialize the Backend using the circuit bytecode
-// 4. Create The Inputs
-// 5. Execute the circuit with the inputs to create the witness
-// 6. Generate the proof(Using Backend) via witness
-// 7. Return the proof
+const CircuitPath = path.resolve(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "../../Circuits/target/ZK_Panagram.json"
+);
+
+const Circuit = JSON.parse(fs.readFileSync(CircuitPath, "utf-8"));
+
+export default async function generateProof() {
+  const inputArray = process.argv.slice(2);
+  try {
+    // 2. Initialize Noir with the circuit
+    const noir = new Noir(Circuit);
+    // 3. Initialize the Backend using the circuit bytecode
+    const bb = new UltraHonkBackend(Circuit.bytecode, { threads: 1 });
+    // 4. Create The Inputs
+    const inputs = {
+      answer_hash: inputArray[0],
+      guess_hash: inputArray[1],
+    };
+    // 5. Execute the circuit with the inputs to create the witness
+    const { witness } = await noir.execute(inputs);
+    // 6. Generate the proof(Using Backend) via witness
+    const OrigCons = console.log;
+    console.log = () => {}; // Suppress output from the backend
+    const { proof } = await bb.generateProof(witness, { keccak: true });
+    console.log = OrigCons; // Restore original console output
+    // 7. ABI Encode the proof for use in smart contracts
+    const abiEncodedProof = ethers.AbiCoder.defaultAbiCoder().encode(
+      ["bytes"],
+      [proof]
+    );
+    // 8. Return the proof
+    return abiEncodedProof;
+  } catch (error) {
+    console.error("Error generating proof:", error);
+    throw error;
+  }
+}
+
+(async () => {
+  generateProof()
+    .then((proof) => {
+      process.stdout.write(proof);
+    })
+    .catch((error) => {
+      console.error("Error in proof generation:", error);
+    });
+})();
--- contract/test/Panagram.t.sol
+++ contract/test/Panagram.t.sol
@@ -8,6 +8,8 @@ import {HonkVerifier} from "../src/PanagramBerifier.sol";
 contract PanagramTest is Test {
     HonkVerifier public verifier;
     Panagram public panagram;
+    address public User = makeAddr("User");
+    address public SecondUser = makeAddr("SecondUser");
     uint256 public constant FIELD_MODULUS =
         21888242871839275222246405745257275088548364400416034343698204186575808495617;
     bytes32 answer = bytes32(uint256(keccak256("DeathGod")) % FIELD_MODULUS);
@@ -24,7 +26,7 @@ contract PanagramTest is Test {
         panagram.startRound(answer);
     }
 
-    function getProof(bytes32 guess, bytes32 CorrectAnswer) internal {
+    function getProof(bytes32 guess, bytes32 CorrectAnswer) internal returns (bytes memory proof) {
         uint256 MAX_INPUTS = 5;
         string[] memory inputs = new string[](MAX_INPUTS);
         inputs[0] = "npx";
@@ -33,7 +35,34 @@ contract PanagramTest is Test {
         inputs[3] = vm.toString(guess);
         inputs[4] = vm.toString(CorrectAnswer);
 
-        bytes memory proof = vm.ffi(inputs);
+        bytes memory encodedProof = vm.ffi(inputs);
+        proof = abi.decode(encodedProof, (bytes));
+        console.logBytes(proof);
         // return proof;
+    }
+
+    function testPanagramGuess() public {
+        vm.prank(User);
+        bytes memory proof = getProof(answer, answer);
+        panagram.makeGuess(proof);
+
+        vm.assertEq(panagram.balanceOf(User, 0), 1, "User should have 1 token after correct guess");
+        vm.assertEq(panagram.balanceOf(User, 1), 0, "User should have 1 token after correct guess");
+
+        vm.prank(User);
+        vm.expectRevert();
+        panagram.makeGuess(proof);
+    }
+
+    function testPanagramSecondGuess() public {
+        vm.prank(User);
+        bytes memory proof = getProof(answer, answer);
+        panagram.makeGuess(proof);
+
+        vm.assertEq(panagram.balanceOf(User, 0), 1, "User should have 1 token after correct guess");
+        vm.assertEq(panagram.balanceOf(User, 1), 0, "User should have 1 token after correct guess");
+
+        vm.prank(SecondUser);
+        panagram.makeGuess(proof);
     }
 }
